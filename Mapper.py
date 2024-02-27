dff76bd18e23310925a45cf603cad9814fcace45
Kartik Upadhyay <37153644+AaronStone24@users.noreply.github.com>
2024-02-27 16:47:09 +0530

    Completed Mapping from OLTP to Landing for data loading

--- Mapper.py
+++ Mapper.py
@@ -4,6 +4,7 @@ import numpy as np
 from Constants import SRC_SCHEMA, TGT_SCHEMA
 
 # TODO: Error Handling
+# TODO: Check if the OLAP table is already filled or not
 
 def oltp_to_olap_mapping(oltp_table_name, olap_table_name, mapping: dict, conn):
     '''
@@ -16,6 +17,8 @@ def oltp_to_olap_mapping(oltp_table_name, olap_table_name, mapping: dict, conn):
     oltp_columns, olap_columns = list(mapping.keys()), list(mapping.values())
 
     sql_query = f'SELECT {','.join(oltp_columns)} FROM {SRC_SCHEMA}.{oltp_table_name}'
+    if olap_table_name == 'Calendar_Dim':
+        sql_query = f'SELECT DISTINCT {oltp_columns[0]} FROM {SRC_SCHEMA}.{oltp_table_name}'
     oltp_df = pd.read_sql_query(sql_query, conn)
     print(oltp_df.head())
 
@@ -25,9 +28,76 @@ def oltp_to_olap_mapping(oltp_table_name, olap_table_name, mapping: dict, conn):
     # Rename the columns
     oltp_df = oltp_df.rename(columns=mapping)
 
+    # Special case for Calendar_Dim
+    if olap_table_name == 'Calendar_Dim':
+        oltp_df['DayofTheWeek'] = oltp_df['FullDate'].dt.day_name()
+        oltp_df['DayType'] = np.where(oltp_df['DayofTheWeek'].isin(['Saturday', 'Sunday']), 'Weekend', 'Weekday')
+        oltp_df['DayofTheMonth'] = oltp_df['FullDate'].dt.day
+        oltp_df['Month'] = oltp_df['FullDate'].dt.month
+        oltp_df['Quarter'] = oltp_df['FullDate'].dt.quarter
+        oltp_df['Year'] = oltp_df['FullDate'].dt.year
+
     # Add the SourceTable column to the DataFrame
     oltp_df['SourceTable'] = f'{SRC_SCHEMA}.{oltp_table_name}'
+    # Add the loadTimeDate column to the DataFrame
+    oltp_df['loadTimeDate'] = pd.to_datetime('now')
 
     # Write the data to the OLAP database
-    oltp_df.to_sql(olap_table_name, conn, schema=TGT_SCHEMA, if_exists='replace', index=False)
+    oltp_df.to_sql(olap_table_name, conn, schema=TGT_SCHEMA, if_exists='append', index=False)
+    conn.commit()
+
+def customerEmployee_Fact_mapper(conn):
+    sql_query = '''SELECT CustomerKey, EmployeeKey, CalendarKey, OD.OrderID,
+        SUM(UnitPrice * (1 - Discount) * Quantity) AS Sales
+        FROM {0}.Orders OD
+        JOIN {1}.Customer_Dim CD
+        ON OD.CustomerID = CD.CustomerID
+        JOIN {1}.Employee_Dim ED
+        ON OD.EmployeeID = ED.EmployeeID
+        JOIN {1}.Calendar_Dim CLD
+        ON OD.OrderDate = CLD.FullDate
+        JOIN {0}.[Order Details] ODT
+        ON OD.OrderID = ODT.OrderID
+        GROUP BY CustomerKey, EmployeeKey, CalendarKey, OD.OrderID'''.format(SRC_SCHEMA, TGT_SCHEMA)
+    
+    df = pd.read_sql_query(sql_query, conn)
+    print(df.head())
+
+    # Replace the missing values with NaN
+    df.replace('', np.NaN, inplace=True)
+
+    # Add the SourceTable column to the DataFrame
+    df['SourceTable'] = f'{SRC_SCHEMA}.Orders, {SRC_SCHEMA}.OrderDetails'
+    # Add the loadTimeDate column to the DataFrame
+    df['loadTimeDate'] = pd.to_datetime('now')
+
+    # Write the data to the OLAP database
+    df.to_sql('CustomerEmployee_Fact', conn, schema=TGT_SCHEMA, if_exists='append', index=False)
+    conn.commit()
+
+def ProductInStock_Fact_mapper(conn):
+    sql_query = '''SELECT CalendarKey, ProductKey, CategoriesKey, SupplierKey, UnitsInStock, UnitsOnOrder, ReorderLevel,
+    SUM(Quantity) OVER (PARTITION BY ODT.ProductID) AS TotalQuantity,
+    OD.OrderID
+    FROM {0}.Orders OD
+    JOIN {0}.[Order Details] ODT ON OD.OrderID = ODT.OrderID
+    JOIN {0}.Products PDT ON ODT.ProductID = PDT.ProductID
+    JOIN {1}.Calendar_Dim CLD ON OD.OrderDate = CLD.FullDate
+    JOIN {1}.Product_Dim PD ON ODT.ProductID = PD.ProductID
+    JOIN {1}.Categories_Dim CD ON PDT.CategoryID = CD.CategoryID
+    JOIN {1}.Supplier_Dim SD ON PDT.SupplierID = SD.SupplierID'''.format(SRC_SCHEMA, TGT_SCHEMA)
+
+    df = pd.read_sql_query(sql_query, conn)
+    print(df.head())
+
+    # Replace the missing values with NaN
+    df.replace('', np.NaN, inplace=True)
+
+    # Add the SourceTable column to the DataFrame
+    df['SourceTable'] = f'{SRC_SCHEMA}.Orders, {SRC_SCHEMA}.OrderDetails, {SRC_SCHEMA}.Products'
+    # Add the loadTimeDate column to the DataFrame
+    df['loadTimeDate'] = pd.to_datetime('now')
+
+    # Write the data to the OLAP database
+    df.to_sql('ProductInStock_Fact', conn, schema=TGT_SCHEMA, if_exists='append', index=False)
     conn.commit()
--- OLTPtoOLAP.py
+++ OLTPtoOLAP.py
@@ -4,7 +4,7 @@ import pyodbc as odbc
 from sqlalchemy import create_engine
 
 from Constants import CONNECTION_URL, SRC_SCHEMA, TGT_SCHEMA
-from Mapper import oltp_to_olap_mapping
+from Mapper import oltp_to_olap_mapping, customerEmployee_Fact_mapper, ProductInStock_Fact_mapper
 
 # TODO: Logging and Error Handling
 # TODO: Consider using a configuration file for the connection strings
@@ -18,9 +18,9 @@ with engine.connect() as conn:
     # Mapping the OLTP tables to OLAP tables
     '''
     OLTP: Products(ProductID, ProductName, UnitPrice, Discontinued)
-    OLAP: Product_dim(ProductKey, ProductID, ProductName, UnitPrice, Discontinued)
+    OLAP: Product_Dim(ProductKey, ProductID, ProductName, UnitPrice, Discontinued)
     One to one mapping of columns from OLTP to OLAP
-    ''' 
+    '''
     oltp_to_olap_mapping(
         'Products',
         'Product_Dim', 
@@ -29,6 +29,68 @@ with engine.connect() as conn:
     )
 
     '''
-    OLTP: Suppliers()
+    OLTP: Suppliers(SupplierID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone)
+    OLAP: Supplier_Dim(SupplierKey, SupplierID, CompanyName, ContactName, ContactTitle, SupplierAddress, City, Region, PostalCode, Country, Phone, loadTimeDate, SourceTable)
     '''
+    oltp_to_olap_mapping(
+        'Suppliers',
+        'Supplier_Dim', 
+        {'SupplierID': 'SupplierID', 'CompanyName': 'CompanyName', 'ContactName': 'ContactName', 'ContactTitle': 'ContactTitle', 'Address': 'SupplierAddress', 'City': 'City', 'Region': 'Region', 'PostalCode': 'PostalCode', 'Country': 'Country', 'Phone': 'Phone'},
+        conn
+    )
 
+    '''
+    OLTP: Categories(CategoryID, CategoryName, Description)
+    OLAP: Category_Dim(CategoriesKey, CategoryID, CategoryName, CatDescription, loadTimeDate, SourceTable)
+    '''
+    oltp_to_olap_mapping(
+        'Categories',
+        'Categories_Dim', 
+        {'CategoryID': 'CategoryID', 'CategoryName': 'CategoryName', 'Description': 'CatDescription'},
+        conn
+    )
+
+    '''
+    OLTP: Orders(OrderDate)
+    OLAP: Calendar_Dim(CalendarKey, FullDate, DayofTheWeek, DayType, DayofTheMonth, Month, Quarter, Year, loadTimeDate, SourceTable)
+    '''
+    oltp_to_olap_mapping(
+        'Orders',
+        'Calendar_Dim', 
+        {'OrderDate': 'FullDate'},
+        conn
+    )
+
+    '''
+    OLTP: Customers(CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone)
+    OLAP: Customer_Dim(CustomerKey, CustomerID, CompanyName, ContactName, ContactTitle, CustAddress, City, Region, PostalCode, Country, Phone, loadTimeDate, SourceTable)
+    '''
+    oltp_to_olap_mapping(
+        'Customers',
+        'Customer_Dim', 
+        {'CustomerID': 'CustomerID', 'CompanyName': 'CompanyName', 'ContactName': 'ContactName', 'ContactTitle': 'ContactTitle', 'Address': 'CustAddress', 'City': 'City', 'Region': 'Region', 'PostalCode': 'PostalCode', 'Country': 'Country', 'Phone': 'Phone'},
+        conn
+    )
+
+    '''
+    OLTP: Employees(EmployeeID, LastName, FirstName, BirthDate, HireDate, Region, Country)
+    OLAP: Employee_Dim(EmployeeKey, EmployeeID, LastName, FirstName, BirthDate, HireDate, Region, Country, loadTimeDate, SourceTable)
+    '''
+    oltp_to_olap_mapping(
+        'Employees',
+        'Employee_Dim', 
+        {'EmployeeID': 'EmployeeID', 'LastName': 'LastName', 'FirstName': 'FirstName', 'BirthDate': 'BirthDate', 'HireDate': 'HireDate', 'Region': 'Region', 'Country': 'Country'},
+        conn
+    )
+    
+    '''
+    OLTP: Orders(OrderID), OrderDetails(OrderID, UnitPrice, Quantity, Discount)
+    OLAP: CustomerEmployee_Fact(CustomerKey, EmployeeKey, CalendarKey, OrderID, Sales, loadTimeDate, SourceTable)
+    '''
+    customerEmployee_Fact_mapper(conn)
+
+    '''
+    OLTP: Orders(OrderID), Order Details(ProductID, Quantity), Products(UnitsInStock, UnitsOnOrder, ReorderLevel)
+    OLAP: ProductInStock_Fact(CalendarKey, ProductKey, CategoriesKey, SupplierKey, UnitsInStock, UnitsOnOrder, ReorderLevel, TotalQuantity, OrderID, loadTimeDate, SourceTable)
+    '''
+    ProductInStock_Fact_mapper(conn)
